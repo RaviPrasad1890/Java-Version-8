6224d55127606642f96847c55971a5c095d3bda5
RaviPrasad1890 <ravi.prasad.1890@gmail.com>
2020-06-10 19:44:43 +0530

    Imp Concepts

--- java_version_8/src/com/ravi/StaticMethodInsideInterface/Interf.java
+++ java_version_8/src/com/ravi/StaticMethodInsideInterface/Interf.java
@@ -4,8 +4,25 @@ public interface Interf {
     public static void staticMethodOfInterf(){
         System.out.println("Interface Static Method");
     }
+
+    public static void main(String[] args){
+        System.out.println("Interface Main Method");
+    }
+
+    /*
+    deafult static void defaultstatic(){
+        //We can't declare both together
+    }
+    */
 }
 /*
 Generally Utility methods are declared as static method. These methods can be called just by Interface name
-Interface Static method by default not available to child class
+Interface Static method by default not available to child class.
+
+From 1.8v, we can declare main method in interface and can run interface directly from command prompt.
+
+Deafult keyword is mandatory for default methods, whereas for static method,
+public is optional and by default it is always public.
+
+Method with default keyword is allowed only in interface but not in class.
  */
